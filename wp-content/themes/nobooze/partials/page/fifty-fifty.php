eac2f215b2b80796ffd917e1a5bd4053adb0f0c4
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-04-25 16:07:15 +0200

    FEAT: Changes to the Fifty Fifty component + Info Row component

--- wp-content/themes/nobooze/partials/page/fifty-fifty.php
+++ wp-content/themes/nobooze/partials/page/fifty-fifty.php
@@ -1,31 +1,30 @@
 <?php
 
-$modifier = get_sub_field("modifier") ? get_sub_field("modifier") : [];
-$image = get_sub_field("image") ? get_sub_field("image") : "";
 $title = get_sub_field("title") ? get_sub_field("title") : "";
+$image = get_sub_field("image") ? get_sub_field("image") : "";
 $text = get_sub_field("text") ? get_sub_field("text") : "";
 $cta = get_sub_field("cta") ? get_sub_field("cta") : "";
 
 ?>
 
 
-<section class="fifty p-relative d-flex d-flex-wrap <?php foreach ($modifier as $value) { echo(' ' . $value); } ?>">
-    <div class="fifty__wrapper p-relative d-flex d-flex-wrap">
-        <?php if($image): ?>
-            <div class="fifty__image">
-                <img src="<?= $image["url"] ?>" alt="<?= $image["alt"] ?>" class="fifty__img">
-            </div> 
-        <?php endif; ?>
+<section class="fifty p-relative d-flex d-flex-wrap">
+    <?php if($title): ?>
+        <h2 class="fifty__title p-absolute heading-about"><?= $title ?></h2>
+    <?php endif; ?>
+    
+    <?php if($image): ?>
+        <div class="fifty__image">
+            <img src="<?= $image["url"] ?>" alt="<?= $image["alt"] ?>" class="fifty__img">
+        </div> 
+    <?php endif; ?>
+    <div class="fifty__wrapper wrapper-full p-relative d-flex d-flex-wrap w-100"> 
         <div class="fifty__container d-flex">
             <div class="fifty__content">
-                <?php if($title): ?>
-                    <p class="fifty__title f-heading heading-fifty m-reset"><?= $title ?></p>
-                <?php endif; ?>
-    
                 <?php if($text): ?>
                     <div class="fifty__text line-half"><?= $text ?></div>
                 <?php endif; ?>
-    
+                    
                 <?php if($cta): ?>
                     <div class="fifty__btn w-100"> 
                         <a href="<?= $cta["url"] ?>" target="<?= $cta["target"]?>" class="fifty__cta u-btn"><?= $cta["title"]?></a>
---
+++ wp-content/themes/nobooze/partials/page/info-row.php
@@ -0,0 +1,28 @@
+<?php
+$image = get_sub_field("image") ? get_sub_field("image") : "";
+$text = get_sub_field("text") ? get_sub_field("text") : "";
+$cta = get_sub_field("cta") ? get_sub_field("cta") : "";
+?>
+
+<section class="info-row p-relative d-flex d-flex-wrap">
+    <div class="info-row__wrapper wrapper-full p-relative d-flex d-flex-wrap d-flex-align-c w-100"> 
+        <div class="info-row__container d-flex">
+            <div class="info-row__content">
+                <?php if($text): ?>
+                    <div class="info-row__text line-half"><?= $text ?></div>
+                <?php endif; ?>
+                    
+                <?php if($cta): ?>
+                    <div class="info-row__btn w-100"> 
+                        <a href="<?= $cta["url"] ?>" target="<?= $cta["target"]?>" class="info-row__cta u-btn"><?= $cta["title"]?></a>
+                    </div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+    <?php if($image): ?>
+        <div class="info-row__image">
+            <img src="<?= $image["url"] ?>" alt="<?= $image["alt"] ?>" class="info-row__img">
+        </div> 
+    <?php endif; ?>
+</section>
