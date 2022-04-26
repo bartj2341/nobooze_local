952f7b195ed00ac45750fbb8fd942f59b6bb5ddf
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-04-26 14:37:33 +0200

    FEAT: Standard Row component

---
+++ wp-content/themes/nobooze/partials/page/standard-row.php
@@ -0,0 +1,27 @@
+<?php
+
+$image = get_sub_field("image") ? get_sub_field("image") : "";
+$title = get_sub_field("title") ? get_sub_field("title") : "";
+$text = get_sub_field("text") ? get_sub_field("text") : "";
+
+?>
+
+<section class="standard-row p-relative d-flex d-flex-wrap">
+    <?php if($image): ?>
+        <div class="standard-row__image">
+            <img src="<?= $image["url"] ?>" alt="<?= $image["alt"] ?>" class="standard-row__img">
+        </div> 
+    <?php endif; ?>
+    <div class="standard-row__wrapper wrapper-xlarge p-relative d-flex d-flex-wrap d-flex-align-c w-100"> 
+        <div class="standard-row__container d-flex">
+            <div class="standard-row__content">
+                <?php if($title): ?>
+                    <p class="standard-row__title f-heading heading-standard"><?= $title ?></p>
+                <?php endif; ?>
+                <?php if($text): ?>
+                    <div class="standard-row__text line-half"><?= $text ?></div>
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</section>
