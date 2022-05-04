b153bc92ff1ff3172d3c9b5faa29f5a2f204fd53
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-05-04 15:50:40 +0200

    FEAT: Articles Template skeleton

---
+++ wp-content/themes/nobooze/template-articles.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Template name: Articles
+ */
+
+get_header(); ?>
+
+    <main id="main" role="main">
+        <?php nobooze_layout(); ?>
+    </main><!-- #main -->
+<?php
+get_footer();
