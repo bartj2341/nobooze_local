33bfd4fe298e6193734ba929c5a0eb98b849716b
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-05-06 16:53:56 +0200

    FEAT: Desktop Menu (skeleton)

--- wp-content/themes/nobooze/header.php
+++ wp-content/themes/nobooze/header.php
@@ -22,10 +22,42 @@
 </head>
 
 <body <?php body_class(); ?>>
-	<header id="masthead" class="site-header">
-			<nav id="header__navigation" class="main-navigation">
-				This is header.
-			</nav><!-- #site-navigation -->
+	<header id="masthead" class="u-header">
+		<div class="p-relative">
+			<div class="bg-black">
+				<div class="wrapper-full">
+					<div class="u-header__top__content d-flex d-flex-align-c d-flex-justify-e">
+						<div class="c-white f-heading line-reset">Call us: <span class="phone-number">+123 0 123 1234</span></div>
+					</div>
+				</div>
+			</div>
+			<div class="bg-white">
+				<div class="wrapper-full">
+					<div class="u-header__main d-flex d-flex-align-c d-flex-justify-s">
+						<a class="u-header__logo" href="<?= home_url(); ?>"></a>
+						<?php // Remove template template-landing-ebook.php' ?>
+						<?php if( 'lp' != get_post_type() && 'ty' != get_post_type() ): ?>
+						<ul class="u-header__ul p-relative list-reset">
+							<li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative" href="#">HOME</a></li>
+
+							<li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative" href="#">ABOUT</a></li>
+
+							<li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative" href="#">WORK WITH ME</a></li>
+
+							<li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative" href="#">MASTERCLASS</a></li>
+
+							<li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative" href="#">RESOURCES</a></li>
+
+							<li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative" href="#">PRESS & MEDIA</a></li>
+
+							<li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative" href="#">CONTACT</a></li>
+						</ul>
+						<?php endif ?>
+					</div>
+				</div>
+			</div>
+		</div>
+
 	</header><!-- #masthead -->
 
 	
