08585e3a3746b873afb40eb044f460f8058114f4
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-05-09 14:53:39 +0200

    FEAT: Mobile Menu (html and css)

---
+++ wp-content/themes/nobooze/menu-parts/mobile-menu.php
@@ -0,0 +1,82 @@
+<!-- Mobile Menu -->
+<div class="m-menu bg-white w-100">
+    <div class="m-menu__inner">
+        <div class="wrapper-nav">
+            <ul class="m-menu__ul list-reset">
+                <li class="m-menu__li p-relative">
+                    <a class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer" href="<?= home_url() ?>">
+                        <span class="text-contact">HOME</span>
+                    </a>
+                </li>
+                <li class="m-menu__li p-relative">
+                    <a class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer" href="<?= site_url() ?>/about">
+                        <span class="text-contact">ABOUT</span>
+                    </a>
+                </li>
+                <li class="m-menu__li m-menu__has-children p-relative">
+                    <div class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer">
+                        <span class="text-contact f-heading">WORK WITH ME</span>
+                        <div class="m-menu__li__arrow d-block"></div>
+                    </div>
+                    <ul class="m-submenu__ul list-reset p-relative">
+                        <li class="m-submenu__li">
+                            <a class="d-flex d-flex-align-c w-100" href="#">
+                                <div class="m-submenu__text f-heading">DISCOVER PROGRAM</div>
+                            </a>
+                        </li>
+                        <li class="m-submenu__li">
+                            <a class="d-flex d-flex-align-c w-100" href="#">
+                                <div class="m-submenu__text f-heading">FREEDOM COACHING PROGRAM</div>
+                            </a>
+                        </li>
+                        <li class="m-submenu__li">
+                            <a class="d-flex d-flex-align-c w-100" href="<?= site_url() ?>/exclusive-private-coaching">
+                                <div class="m-submenu__text f-heading">EXCLUSIVE PRIVATE COACHING</div>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="m-menu__li p-relative">
+                    <a class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer" href="#">
+                        <span class="text-contact">MASTERCLASS</span>
+                    </a>
+                </li>
+                <li class="m-menu__li m-menu__has-children p-relative">
+                    <div class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer">
+                        <span class="text-contact f-heading">RESOURCES</span>
+                        <div class="m-menu__li__arrow d-block"></div>
+                    </div>
+                    <ul class="m-submenu__ul list-reset p-relative">
+                        <li class="m-submenu__li">
+                            <a class="d-flex d-flex-align-c w-100" href="<?= site_url() ?>/books">
+                                <div class="m-submenu__text f-heading">BOOKS</div>
+                            </a>
+                        </li>
+                        <li class="m-submenu__li">
+                            <a class="d-flex d-flex-align-c w-100" href="<?= site_url() ?>/videos">
+                                <div class="m-submenu__text f-heading">VIDEOS</div>
+                            </a>
+                        </li>
+                        <li class="m-submenu__li">
+                            <a class="d-flex d-flex-align-c w-100" href="<?= site_url() ?>/articles">
+                                <div class="m-submenu__text f-heading">ARTICLES</div>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="m-menu__li p-relative">
+                    <a class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer" href="<?= site_url() ?>/press-media">
+                        <span class="text-contact">PRESS & MEDIA</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="m-menu__btn">
+            <div class="wrapper-nav">
+                <div class="m-menu__cta__wrapper">
+                    <a class="m-menu__cta u-btn--mobile-lg" href="<?= site_url() ?>/contact">CONTACT</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
