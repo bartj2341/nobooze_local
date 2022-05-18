9b192d15a1040177307de5fb910f90425fb818a0
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-05-18 16:45:26 +0200

    FEAT: Booking Calendar plugin

---
+++ wp-content/themes/nobooze/template-booking.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template name: Booking
+ */
+
+get_header(); 
+?>
+
+<div class="main-wrapper" style="padding-top: 150px;">
+    <?php echo do_shortcode('[CP_APP_HOUR_BOOKING id="3"]') ?> 
+</div>
+
+<?php
+get_footer();
