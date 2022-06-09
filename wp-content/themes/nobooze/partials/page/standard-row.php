ffa034d797ccbc7d231d2f3cd94358f615a3fbfb
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-06-09 16:43:16 +0200

    FEAT: Markup for modifiers in the Standard Row component

--- wp-content/themes/nobooze/partials/page/standard-row.php
+++ wp-content/themes/nobooze/partials/page/standard-row.php
@@ -1,10 +1,11 @@
 <?php
+$modifier = get_sub_field("modifier") ? get_sub_field("modifier") : [];
 $image = get_sub_field("image") ? get_sub_field("image") : "";
 $title = get_sub_field("title") ? get_sub_field("title") : "";
 $text = get_sub_field("text") ? get_sub_field("text") : "";
 ?>
 
-<section class="standard-row p-relative d-flex d-flex-wrap">
+<section class="standard-row p-relative d-flex d-flex-wrap <?php foreach ($modifier as $value) { echo(' ' . $value); } ?>">
     <?php if($image): ?>
         <div class="standard-row__image z-index-2">
             <img src="<?= $image["url"] ?>" alt="<?= $image["alt"] ?>" class="standard-row__img d-block fit-cover">
