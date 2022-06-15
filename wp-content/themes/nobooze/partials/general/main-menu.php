774fae4e703e6be990fea4f2b12bd21b1ddfbde1
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-06-15 13:00:51 +0200

    FEAT: Changes in the menu

--- wp-content/themes/nobooze/partials/general/main-menu.php
+++ wp-content/themes/nobooze/partials/general/main-menu.php
@@ -18,38 +18,25 @@ $pagename = $post ? $post->post_name : '';
                 </a>
 
                 <ul class="u-header__ul p-relative list-reset">
-                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('home', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= home_url() ?>">HOME</a></li>
+                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading">
+                        <a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('home', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= home_url() ?>">HOME
+                    </a></li>
 
-                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('about', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= site_url() ?>/about">ABOUT</a></li>
-
-                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading"><a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('work-with-me', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= site_url() ?>/work-with-me">WORK WITH ME</a></li>
-                    
-                    <!-- RESOURCES START -->
-                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal u-header__li--has-children f-heading">
-                        <a href="#" class="u-header__cta u-header__cta--has-children d-flex d-flex-column d-flex-justify-c d-flex-align-c h-100 p-relative <?= strpos('videos articles', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>">
-                            RESOURCES
-                            <?php include get_icons_directory('icons/arrow.svg') ?>
-                        </a>
-                        <div class="u-header__sub__ul">
-                            <div class="u-header__sub__inner p-relative">
-                                <ul class="u-header__sub__list d-flex d-flex-column list-reset">
-                                    <li class="u-header__sub__li p-relative">
-                                        <a class="u-header__sub__cta d-flex d-flex-align-c" href="<?= site_url() ?>/videos">
-                                            <div class="u-header__sub__icon"><?php include get_icons_directory('icons/video.svg') ?></div>
-                                            <div class="u-header__sub__text">VIDEOS</div>
-                                        </a>
-                                    </li>
-                                    <li class="u-header__sub__li p-relative">
-                                        <a class="u-header__sub__cta d-flex d-flex-align-c" href="<?= site_url() ?>/articles">
-                                            <div class="u-header__sub__icon"><?php include get_icons_directory('icons/articles.svg') ?></div>
-                                            <div class="u-header__sub__text">ARTICLES</div>
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
+                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading">
+                        <a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('about', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= site_url() ?>/about">ABOUT</a>
                     </li>
-                    <!-- RESOURCES END -->
+
+                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading">
+                        <a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('work-with-me', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= site_url() ?>/work-with-me">WORK WITH ME</a>
+                    </li>
+
+                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading">
+                        <a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('videos', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= site_url() ?>/videos">VIDEOS</a>
+                    </li>
+
+                    <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading">
+                        <a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('blog', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= site_url() ?>/blog">BLOG</a>
+                    </li>
 
                     <li class="u-header__li d-flex d-flex-align-c p-relative c-pointer line-normal f-heading">
                         <a class="u-header__cta d-flex d-flex-align-c h-100 p-relative <?= strpos('booking', $pagename) !== false && $pagename !== NULL && $pagename !== "" ? 'u-header__cta--active' : '' ?>" href="<?= site_url() ?>/booking">BOOKING</a>
--- wp-content/themes/nobooze/partials/general/mobile-menu.php
+++ wp-content/themes/nobooze/partials/general/mobile-menu.php
@@ -18,25 +18,15 @@
                         <span class="f-heading">WORK WITH ME</span>
                     </a>
                 </li>
-                <li class="m-menu__li m-menu__has-children p-relative">
-                    <div class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer">
-                        <span class="f-heading">RESOURCES</span>
-                        <div class="m-menu__li__arrow d-block"></div>
-                    </div>
-                    <ul class="m-submenu__ul list-reset p-relative">
-                        <li class="m-submenu__li">
-                            <a class="d-flex d-flex-align-c w-100" href="<?= site_url() ?>/videos">
-                                <div class="u-header__sub__icon"><?php include get_icons_directory('icons/video.svg') ?></div>
-                                <div class="m-submenu__text f-heading text-sixteen">VIDEOS</div>
-                            </a>
-                        </li>
-                        <li class="m-submenu__li">
-                            <a class="d-flex d-flex-align-c w-100" href="<?= site_url() ?>/articles">
-                                <div class="u-header__sub__icon"><?php include get_icons_directory('icons/articles.svg') ?></div>
-                                <div class="m-submenu__text f-heading text-sixteen">ARTICLES</div>
-                            </a>
-                        </li>
-                    </ul>
+                <li class="m-menu__li p-relative">
+                    <a class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer" href="<?= site_url() ?>/videos">
+                        <span class="f-heading">VIDEOS</span>
+                    </a>
+                </li>
+                <li class="m-menu__li p-relative">
+                    <a class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer" href="<?= site_url() ?>/blog">
+                        <span class="f-heading">BLOG</span>
+                    </a>
                 </li>
                 <li class="m-menu__li p-relative">
                     <a class="m-menu__link d-flex d-flex-justify-s d-flex-align-c w-100 c-pointer" href="<?= site_url() ?>/booking">
@@ -48,7 +38,7 @@
         <div class="m-menu__btn">
             <div class="wrapper-nav">
                 <div class="m-menu__cta__wrapper">
-                    <a class="m-menu__cta u-btn" href="<?= site_url() ?>/contact">CONTACT</a>
+                    <a class="m-menu__cta u-btn--nav" href="<?= site_url() ?>/contact">CONTACT</a>
                 </div>
             </div>
         </div>
