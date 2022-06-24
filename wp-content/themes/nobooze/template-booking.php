11c43b9e51205ef77adc9e47a04e4c73058c7ec4
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-06-24 12:21:45 +0200

    FIX: Booking shortcode

--- wp-content/themes/nobooze/template-booking.php
+++ wp-content/themes/nobooze/template-booking.php
@@ -9,7 +9,7 @@ get_header();
 <main class="main-wrapper">
     <section class="booking-section">
         <div class="booking-section__container wrapper-large">
-            <?php echo do_shortcode('[ssa_booking type=nobooze-consultation-phone-call] ') ?> 
+            <?php echo do_shortcode('[ssa_booking type=consultation-phone-call] ') ?> 
         </div>
     </section>
 </main>
