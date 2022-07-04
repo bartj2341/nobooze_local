7105909a6be3759945ca5190d0cfed6f017553e2
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-07-04 16:20:47 +0200

    FEAT: Newsletter + minor changes

--- wp-content/themes/nobooze/partials/general/main-menu.php
+++ wp-content/themes/nobooze/partials/general/main-menu.php
@@ -3,13 +3,6 @@ $post = $wp_query->get_queried_object();
 $pagename = $post ? $post->post_name : '';
 ?>
 <div class="p-relative">
-    <div class="bg-blue">
-        <div class="wrapper-nav">
-            <div class="u-header__top__content d-flex d-flex-align-c d-flex-justify-e">
-                <div class="c-white f-heading line-reset text-small">Call us: <span class="phone-number"><a href="tel:+12311231234">+123 1 123 1234</a></span></div>
-            </div>
-        </div>
-    </div>
     <div class="bg-white">
         <div class="wrapper-nav">
             <div class="u-header__main d-flex d-flex-align-c d-flex-justify-s">
---
+++ wp-content/themes/nobooze/template-newsletter.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Template name: Newsletter
+ */
+
+$title = get_field("title") ? get_field("title") : "";
+$message = get_field("message") ? get_field("message") : ""; 
+
+get_header(); 
+?>
+
+<main class="main-wrapper">
+    <section class="newsletter-message booking-wrapper">
+        <div class="newsletter-message__wrapper wrapper-box">
+            <div class="newsletter-message__container">
+            <?php if($title): ?>
+                <h1 class="newsletter-message__title u-text-center"><?= $title ?></h1>
+            <?php endif; ?>
+
+            <?php if($message): ?>
+                <p class="newsletter-message__text list-basic"><?= $message ?></div>
+            <?php endif; ?>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php 
+get_footer();
