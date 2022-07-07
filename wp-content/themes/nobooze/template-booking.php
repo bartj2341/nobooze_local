57186b9417a5ccc560e931eb4cca1e1d2376e25d
Bartosz Jędrzejewski <bartefix@gmail.com>
2022-07-07 17:08:34 +0200

    FEAT: Form Errors position absolute

--- wp-content/themes/nobooze/template-booking.php
+++ wp-content/themes/nobooze/template-booking.php
@@ -10,7 +10,7 @@ get_header();
     <section class="booking-section booking-wrapper">
         <div class="booking-section__wrapper">
             <div class="booking-section__container">
-                <?php echo do_shortcode('[ssa_booking type=coaching-consultation-call] ') ?> 
+                <?php echo do_shortcode('[ssa_booking type=coaching-consultation-call] ') ?>
             </div>
         </div>
     </section>
